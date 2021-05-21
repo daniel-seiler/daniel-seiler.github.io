9164e5c7eced21a63f19553e568f2b7434e9f1bf
Daniel Seiler <seda1019@h-ka.de>
2021-05-21 12:28:43 +0200

    added drawing of a predefined array

--- javascripts/lines.js
+++ javascripts/lines.js
@@ -9,37 +9,45 @@ const MONOCHROME = [[0, 32, 63]];
 
 function setup(){
     createCanvas(MAX_HEIGHT, MAX_WIDTH);
-    stroke(169, 251, 215);
+    stroke(255, 1, 215);
     strokeWeight(2);
     noLoop();
 }
 
-function draw(){
+function setup() {
+    createCanvas(1000, 1000);
+}
+
+var arrTest = [
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 1, 0.5, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+]
+
+function draw() {
     const lines = [];
-    for(let i = STEP; i < MAX_HEIGHT - STEP; i += STEP){
+
+    for (let i = 0; i < arrTest.length; i++) {
         let line = [];
-        for(let j = STEP; j <= MAX_HEIGHT - STEP; j += STEP){
-            let distanceToCenter = Math.abs(j - MAX_HEIGHT / 2);
-            let variance = Math.max(MAX_HEIGHT / 2 - VARIANCE_FACTOR - distanceToCenter, 0);
-            let random = Math.random() * variance / 2 * -1;
-            let point = {x: j, y: i + random};
-            line.push(point)
+        for (let j = 0; j < arrTest[i].length; j++) {
+            let point = {x: (j + 3) * 50, y: (i + 3) * 50, z: arrTest[i][j] * 50};
+            line.push(point);
         }
         lines.push(line);
     }
 
-
     //draw
-    for(let i = 5; i < lines.length; i++){
+    for(let i = 0; i < arrTest.length; i++){
+        noFill();
         beginShape();
-        for(let j = 0; j < lines[i].length; j+=2){
-            curveVertex(lines[i][j].x, lines[i][j].y);
-            let random_index = Math.floor(Math.random() * MONOCHROME.length);
-            const [r, g, b] = MONOCHROME[random_index];
-            fill(r, g, b);
-            curveVertex(lines[i][j+1].x, lines[i][j+1].y)
+        for(let j = 0; j < arrTest[i].length; j++){
+            curveVertex(lines[i][j].x, lines[i][j].y - lines[i][j].z);
         }
         endShape();
     }
 }
-
