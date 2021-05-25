c8c8e474021fb95f57a8f24747bea0c4cc22fe58
Daniel Seiler <seda1019@h-ka.de>
2021-05-25 21:49:37 +0200

    added js to create point matrix from time

---
+++ javascripts/lines.js
@@ -0,0 +1,61 @@
+function writeCoords(currentTime) {
+    let currentArr = createArr(currentTime);
+    let lines = [];
+
+    for (let i = 0; i < currentArr.length; i++) {
+        let line = [];
+        for (let j = 0; j < currentArr[i].length; j++) {
+            let point = {x: (j + xyOffset) * multFact, y: (i + xyOffset) * multFact, z: currentArr[i][j] * multFact};
+            line.push(point);
+        }
+        lines.push(line);
+    }
+
+    return lines;
+}
+
+function createArr(currentTime) {
+    let currentArr = arrSpaceChar;
+    for (let i = 0; i < currentTime.length; i++) {
+        appendArr(currentArr, charToArr(currentTime.charAt(i)));
+        //appendArr(currentArr, arrSpaceChar);//TODO space seems to copy previous array
+    }
+    return currentArr;
+}
+
+function charToArr(character) {
+    switch (character) {
+        case '0':
+            return arr0;
+        case '1':
+            return arr1;
+        case '2':
+            return arr2;
+        case '3':
+            return arr3;
+        case '4':
+            return arr4;
+        case '5':
+            return arr5;
+        case '6':
+            return arr6;
+        case '7':
+            return arr7;
+        case '8':
+            return arr8;
+        case '9':
+            return arr9;
+        case ':':
+            return arrColon;
+        default:
+            console.log("default charToArr");
+            return arrColon;
+    }
+}
+
+function appendArr(currentArr, newArr) {
+    console.log(newArr);
+    for (let i = 0; i < currentArr.length; i++) {
+        currentArr[i].push(...newArr[i]);
+    }
+}
