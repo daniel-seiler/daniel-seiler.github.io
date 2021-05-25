30f163a28734ed7b37a98900bdea0f0df72343a2
Daniel Seiler <seda1019@h-ka.de>
2021-05-25 21:53:29 +0200

    removed old code

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,33 +1,12 @@
 const MAX_HEIGHT = 300;
-const STEP = 16;
-
-const VARIANCE_FACTOR = 100;
-const TONES_1 = [[238, 66, 102], [31, 64, 104], [242, 228, 181]];
-const TONES_2 = [[37, 106, 220], [31, 64, 104], [169, 251, 215]];
-const MONOCHROME = [[0, 32, 63]];
 
 function setup(){
     let cnv = createCanvas(windowWidth, MAX_HEIGHT);
     cnv.parent('clock-holder');
     stroke(8, 255, 131);
     frameRate(2);
-    strokeWeight(2);//default is 1
+    strokeWeight(2);
 }
-
-/*var timeArr = arrColon;
-[
-[0, 0, 0, 0, 1, 1, 0, 0, 0],
-[0, 0, 0, 1, 1, 1, 0, 0, 0],
-[0, 0, 1, 1, 1, 1, 0, 0, 0],
-[0, 0, 0, 0, 1, 1, 0, 0, 0],
-[0, 0, 0, 0, 1, 1, 0, 0, 0],
-[0, 0, 0, 0, 1, 1, 0, 0, 0],
-[0, 0, 0, 0, 1, 1, 0, 0, 0],
-[0, 0, 0, 0, 1, 1, 0, 0, 0],
-]*/
-
-var multFact = 3;//5
-var xyOffset = 2;
 
 var currentTime = "00:00";
 var nextTime = "00:00";
