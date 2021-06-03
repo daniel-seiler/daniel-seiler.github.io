0b29a67ae2f96da275d7d89994c21c673253ad7c
Daniel Seiler <seda1019@h-ka.de>
2021-06-03 22:04:59 +0200

    prepare js for multiple p5 sketches

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,37 +1,40 @@
-const MAX_HEIGHT = 180;
-const MAX_WIDTH = 735;
-const Z_AXIS_OFFSET = 0.9;
+var drawClock = function(p) {
+    const MAX_HEIGHT = 180;
+    const MAX_WIDTH = 735;
+    const Z_AXIS_OFFSET = 0.9;
+    let currentTime = "00:00";
+    let nextTime = "00:00";
+    let lines = [];
 
-function setup(){
-    let cnv = createCanvas(MAX_WIDTH, MAX_HEIGHT);
-    cnv.parent('clock-holder');
-    stroke(8, 255, 131);
-    strokeWeight(2);
-}
+    p.setup = function(){
+        p.createCanvas(MAX_WIDTH, MAX_HEIGHT);
+        //p.parent('clock-holder');
+        p.stroke(8, 255, 131);
+        p.strokeWeight(2);
+    };
 
-let currentTime = "00:00";
-let nextTime = "00:00";
-let lines = [];
-
-function draw() {
-    let date = new Date();
-    let hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
-    let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
-    nextTime = hours + ":" + minutes;
-    if (nextTime !== currentTime) {
-        currentTime = nextTime;
-        lines = [];
-        lines = writeCoords(currentTime);
-    }
-
-    //draw
-    clear();
-    for(let i = 0; i < lines.length; i++){
-        noFill();
-        beginShape();
-        for(let j = 0; j < lines[i].length; j++){
-            curveVertex(lines[i][j].x, lines[i][j].y - Z_AXIS_OFFSET * lines[i][j].z);
+    p.draw = function() {
+        let date = new Date();
+        let hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
+        let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+        nextTime = hours + ":" + minutes;
+        if (nextTime !== currentTime) {
+            currentTime = nextTime;
+            lines = [];
+            lines = writeCoords(currentTime);
         }
-        endShape();
-    }
-}
+
+        //draw clock
+        p.clear();
+        for(let i = 0; i < lines.length; i++){
+            p.noFill();
+            p.beginShape();
+            for(let j = 0; j < lines[i].length; j++){
+                p.curveVertex(lines[i][j].x, lines[i][j].y - Z_AXIS_OFFSET * lines[i][j].z);
+            }
+            p.endShape();
+        }
+    };
+};
+
+var p5Clock = new p5(drawClock, 'clock-holder');
